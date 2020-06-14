bc57fed6c8150aa4681c2297cfa56b064618dc8b
Edson Simoes Boldrini <edsonboldrini@gmail.com>
2020-06-14 00:34:10 -0300

    adding sign_up page

--- lib/app/modules/auth/sign_up/sign_up_page.dart
+++ lib/app/modules/auth/sign_up/sign_up_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:truckin/app/modules/shared/utils/styles.dart';
 import 'sign_up_controller.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -16,11 +17,80 @@ class _SignUpPageState extends ModularState<SignUpPage, SignUpController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height * 0.33,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text('LOGO'),
+                    Container(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.33,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      'Qual e seu numero de telefone?',
+                    ),
+                    Container(
+                      height: 12,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: '(XX) XXXXX-XXXX',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    Container(
+                      height: 12,
+                    ),
+                    RaisedButton(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      onPressed: () => print('sign_in'),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.33,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    RaisedButton(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      onPressed: () => print('facebook'),
+                      color: CustomColors.blueFacebook,
+                      child: Text(
+                        'Entrar com Facebook',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
